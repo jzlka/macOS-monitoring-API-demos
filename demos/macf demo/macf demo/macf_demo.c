03a8ddddb3086f4380286aebb8a1299e4ab470bb
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2020-05-29 22:50:10 +0200

    macf changed to generic kext

--- demos/macf demo/macf demo/macf_demo.c
+++ demos/macf demo/macf demo/macf_demo.c
@@ -1,24 +1,12 @@
-#include <IOKit/IOLib.h>
-
-extern "C" {
+#include <sys/systm.h>
+#include <mach/mach_types.h>
 #include <Kernel/security/mac_policy.h>
-}
-
-#include "macf_demo.hpp"
-
-// This required macro defines the class's constructors, destructors,
-// and several other methods I/O Kit requires.
-OSDefineMetaClassAndStructors(com_jzlka_driver_MACF_demo, IOService)
-
-// Define the driver's superclass.
-#define super IOService
-
+#include "macf_demo.h"
 
 static int openCallaback(kauth_cred_t cred, struct vnode *vp, struct label *label, int acc_mode)
 {
 
 }
-
 
 static struct mac_policy_ops mmPolicyOps = {
     .mpo_vnode_check_open_t = openCallback,
@@ -39,71 +27,24 @@ static struct mac_policy_conf policyConf = {
 
 static mac_policy_handle_t handlep;
 
-
-
-bool com_jzlka_driver_MACF_demo::init(OSDictionary *dict)
-{
-    IOLog("(%s)_init Hello, World!\n", m_demoName);
-    if (!super::init(dict)) {
-        IOLog("%s: super::init() failed.\n", m_demoName);
-        return false;
-    }
-
-    IOLog("%s: Point of interest: %s\n", m_demoName, m_demoPath);
-
-
-    void *ctx;
-    int status = mac_policy_register(&policyConf, &handlep, ctx);
-    if (status != KERN_SUCCESS) {
-        IOLog("%s: failed to register MAC policy (%#x)\n", m_demoName, status);
-        return false;
-    }
-
-    return true;
-}
-
-void com_jzlka_driver_MACF_demo::free(void)
-{
-    IOLog("(%s)_free Goodbye, World!\n", m_demoName);
-    super::free();
-    return mac_policy_unregister(handlep); // Deregistering the policy
-}
-
-IOService *com_jzlka_driver_MACF_demo::probe(IOService *provider,
-    SInt32 *score)
-{
-    IOService *result = super::probe(provider, score);
-    IOLog("%s: Probing\n", m_demoName);
-    return result;
-}
-
-bool com_jzlka_driver_MACF_demo::start(IOService *provider)
-{
-    IOLog("%s: Starting\n", m_demoName);
-    if (!super::start(provider)) {
-        IOLog("%s: super::start() failed.\n", m_demoName);
-        return false;
-    }
-    return true;
-}
-
-void com_jzlka_driver_MACF_demo::stop(IOService *provider)
-{
-    IOLog("%s: Stopping", m_demoName);
-    super::stop(provider);
-}
-
-#include <sys/systm.h>
-#include <mach/mach_types.h>
-
 kern_return_t generic_kext_demo_start(kmod_info_t * ki, void *d);
 kern_return_t generic_kext_demo_stop(kmod_info_t *ki, void *d);
-const char* g_demoName = "generic-kext";
+const char* g_demoName = "macf";
+const char* g_demoPath = "/tmp/macf-demo";
 
 // The extension has been loaded. Register your callbacks..
 kern_return_t generic_kext_demo_start(kmod_info_t * ki, void *d)
 {
     printf("(%s) Hello, World!\n", g_demoName);
+    printf("%s: Point of interest: %s\n", g_demoName, g_demoPath);
+
+    void *ctx;
+    int status = mac_policy_register(&policyConf, &handlep, ctx);
+    if (status != KERN_SUCCESS) {
+        IOLog("%s: failed to register MAC policy (%#x)\n", g_demoName, status);
+        return status;
+    }
+
     return KERN_SUCCESS;
 }
 
@@ -111,5 +52,6 @@ kern_return_t generic_kext_demo_start(kmod_info_t * ki, void *d)
 kern_return_t generic_kext_demo_stop(kmod_info_t *ki, void *d)
 {
     printf("(%s) Goodbye, World!\n", g_demoName);
+    return mac_policy_unregister(handlep); // Deregistering the policy
     return KERN_SUCCESS;
 }
