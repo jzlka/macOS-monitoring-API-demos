caac01802e31b16c697c4a63bd94b25197952211
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2020-06-16 12:12:54 +0200

    Escaped ad-hoc certificate.

--- demos/ESF demo/sign.sh
+++ demos/ESF demo/sign.sh
@@ -2,7 +2,7 @@
 APP=ESF-demo
 PATH_TO_APP="ESF demo"
 ENTITLEMENTS="ESF demo.entitlements"
-CERT_ID=-
+CERT_ID="-"
 
 echo "\n\n**** Previous entitlements of $APP ****"
 codesign -d --ent :-  "$PATH_TO_APP/$APP"
